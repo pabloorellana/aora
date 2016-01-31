6e02de0f5dd14c53c80c61d4ed07da60eaff2089
pablo <pablo.orellana.torrez@gmail.com>
2016-01-31 18:14:54 -0400

    Adding authentication middleware for projects

--- routes/projects.js
+++ routes/projects.js
@@ -1,15 +1,36 @@
 var router = require('express').Router(),
+    jwtUtils = require('../utils/jwt.js'),
     projectsController = require('../controllers/projects.js'),
     formatter = require('../utils/formatter.js');
 
-router.post('/', projectsController.save, formatter.formatErrorResponse);
+router.post('/',
+	jwtUtils.verifyToken,
+	projectsController.save,
+	formatter.formatErrorResponse
+);
 
-router.get('/', projectsController.getAll, formatter.formatErrorResponse);
+router.get('/',
+	jwtUtils.verifyToken,
+	projectsController.getAll,
+	formatter.formatErrorResponse
+);
 
-router.get('/:projectId', projectsController.getOne, formatter.formatErrorResponse);
+router.get('/:projectId',
+	jwtUtils.verifyToken,
+	projectsController.getOne,
+	formatter.formatErrorResponse
+);
 
-router.put('/:projectId', projectsController.update, formatter.formatErrorResponse);
+router.put('/:projectId',
+	jwtUtils.verifyToken,
+	projectsController.update,
+	formatter.formatErrorResponse
+);
 
-router.delete('/:projectId', projectsController.remove, formatter.formatErrorResponse);
+router.delete('/:projectId',
+	jwtUtils.verifyToken,
+	projectsController.remove,
+	formatter.formatErrorResponse
+);
 
 module.exports = router;

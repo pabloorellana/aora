10e6f6c3c2317eaf97c237532acb3f621f9fce01
pablo <pablo.orellana.torrez@gmail.com>
2016-02-21 21:14:14 -0400

    Refactoring stories controller

--- controllers/stories.js
+++ controllers/stories.js
@@ -3,85 +3,62 @@
 const Stories = require('../models/story.js');
 
 function save (req, res, next) {
-    Stories.create(req.body).then((story) => {
-        res.json({
-            data: story
-        });
-    }).catch((err) => {
-        res.errorInfo = { status: 500 };
-        next();
+    Stories.create(req.body).then(story => {
+        res.json({ data: story });
+    }).catch(err => {
+        next({ status: 500 });
     })
 };
 
 function getAll (req, res, next) {
     const projectId = req.params.projectId;
 
-    Stories.find({ projectId }).then((stories) => {
-         if (stories.length === 0) {
-            res.errorInfo = { status: 404, title: 'No Stories Found' };
-            return next();
-        }
-
-        res.json({
-            data: stories
-        });
-    }).catch((err) => {
-        res.errorInfo = { status: 500 };
-        next();
+    Stories.find({ projectId }).then(stories => {
+        res.json({ data: stories });
+    }).catch(err => {
+        next({ status: 500 });
     });
 }
 
 function getOne (req, res, next) {
     const storyId = req.params.storyId;
 
-    Stories.findOne({ _id: storyId }).then((story) => {
+    Stories.findOne({ _id: storyId }).then(story => {
         if (!story) {
-            res.errorInfo = { status: 404, title: 'Story not Found' };
-            return next();
+            return next({ status: 404, title: 'Story not Found' });
         }
 
-        res.json({
-            data: story
-        });
-    }).catch((err) => {
-        res.errorInfo = { status: 500 };
-        next();
+        res.json({ data: story });
+    }).catch(err => {
+        next({ status: 500 });
     });
 }
 
 function update (req, res, next) {
     const storyId = req.params.storyId;
 
-    Stories.findByIdAndUpdate(storyId, req.body, { new: true }).then((story) => {
+    Stories.findByIdAndUpdate(storyId, req.body, { new: true }).then(story => {
         if (!story) {
-            res.errorInfo = { status: 404, title: 'Story not Found' };
-            return next();
+            return next({ status: 404, title: 'Story not Found' });
         }
 
-        res.json({
-            data: story
-        });
-    }).catch((err) => {
-        res.errorInfo = { status: 500 };
-        next();
+        res.json({ data: story });
+    }).catch(err => {
+        next({ status: 500 });
     });
 }
 
 function remove (req, res, next) {
     const storyId = req.params.storyId;
 
-    Stories.findByIdAndRemove(storyId).then((story) => {
+    Stories.findByIdAndRemove(storyId).then(story => {
         if (!story) {
-            res.errorInfo = { status: 404, title: 'Story not Found' };
-            return next();
+            return next({ status: 404, title: 'Story not Found' });
         }
 
-        res.json({
-            data: story
-        });
-    }).catch((err) => {
-        res.errorInfo = { status: 500 };
-        next();
+        res.json({ data: story });
+    }).catch(err => {
+        next({ status: 500 });
     });
 }
 
